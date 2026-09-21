bbc47a70c4ff60245924f80374df89523b5cff86
Gabriel Intrieri <gabyamil.gi@gmail.com>
2026-09-21 15:22:59 -0300

    se modifico prueba_aruco ahora se imprimen las coordenadas del aruco en pixles con consola

--- prueba_aruco.py
+++ prueba_aruco.py
@@ -7,7 +7,7 @@ diccionario = cv2.aruco.getPredefinedDictionary(
 parametros = cv2.aruco.DetectorParameters()
 detector = cv2.aruco.ArucoDetector(diccionario, parametros)
 
-camara = cv2.VideoCapture(0)
+camara = cv2.VideoCapture(1)
 
 if not camara.isOpened():
     print("No se pudo abrir la cámara")
@@ -15,7 +15,11 @@ if not camara.isOpened():
 
 print("Camara iniciada.")
 print("Mostrar un marcador ArUco frente a la camara.")
+print("Presionar R para mostrar el plano.")
 print("Presiona ESC para salir.")
+
+esquinas_registradas = None
+id_registrado = None
 
 while True:
 
@@ -32,11 +36,33 @@ while True:
     if ids is not None:
         cv2.aruco.drawDetectedMarkers(frame, esquinas, ids)
 
-    # Mostrar el frame con los marcadores detectados
     cv2.imshow("Cam", frame)
 
-    # Salir si se presiona la tecla ESC
     tecla = cv2.waitKey(1) & 0xFF
+
+    if tecla == ord('r'):
+
+        if ids is None:
+            print("No se puede registrar: no hay ningun aruco registrado.")
+        
+        else:
+            indice = 0
+
+            id_registrado = int(ids[indice])
+
+            esquinas_registradas = esquinas[indice][0].copy()
+
+            print("\n==============================")
+            print("REGISTRO REALIZADO")
+            print("==============================")
+            print(f"ID registrado: {id_registrado}")
+
+            for i, punto in enumerate(esquinas_registradas):
+                x, y = punto
+                print(f"Esquina {i}: ({x:.1f}, {y:.1f})")
+            
+            print("==============================\n")
+
 
     if tecla == 27:
         break
